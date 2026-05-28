2da5d728c020adabda4e547bb4bd2d08baef85b9
Ayandip paul <152761756+Ayandip13@users.noreply.github.com>
2026-05-28 23:49:53 +0530

    add constant colours

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,13 +1,30 @@
 export const COLORS = {
-  primary: '',
-  secondary: '',
+  primary: '#008080',
+  secondary: '#DBEDED',
 
-  background: '',
-  card: '',
+  background: '#F7F9F6',
 
-  text: '',
-  gray: '',
+  text: '#222222',
+  textMuted: '#6B6B6B',
 
-  success: '',
-  error: '',
+  grayLight: '#F9FAFB',
+  grayHeavvy: '#CECECE',
+
+  green: '#10B981',
+  greenlight: '#E2F6F0',
+
+  yellow: '#D97706',
+  yellowlight: '#FEF3C7',
+
+  blue: '#3B82F6',
+  blueLight: '#DBDBFF',
+
+  pink: '#D52DC2',
+  pinklight: '#FCE7F3',
+
+  purple: '#AA5AF8',
+  purplelight: '#F4EAFE',
+
+  red: '#EF4545',
+  redLight: '#FDE8E8',
 };
